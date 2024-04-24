5e42ef1c235eb5a27cd8d8f1dcbb1cc6e45126c8
renpeng <renpeng@hellogroup.com>
2024-04-24 19:03:31 +0800

    add

--- src/main/java/org/example/chapter1/Bag.java
+++ src/main/java/org/example/chapter1/Bag.java
@@ -3,21 +3,48 @@ package org.example.chapter1;
 import java.util.Iterator;
 
 public class Bag<Item> implements Iterable<Item> {
-    public Bag(){}
 
-    void add(Item item){}
-
-    boolean isEmpty(){
-        return false;
+    private class Node{
+        Item item;
+        Node next;
     }
 
-    int size(){
-        return 0;
+    private Node first;
+    private int N;
+
+    public void add(Item item){
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
     }
 
+    private class ListIterator implements Iterator<Item>{
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
 
     @Override
     public Iterator<Item> iterator() {
-        return null;
+        return new ListIterator();
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
     }
 }
