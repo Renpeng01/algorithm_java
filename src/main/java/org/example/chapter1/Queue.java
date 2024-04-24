81e71387d7c147ea6f9ff5883bc0fdd2d05240fb
renpeng <renpeng@hellogroup.com>
2024-04-24 14:24:17 +0800

    add

--- src/main/java/org/example/chapter1/Queue.java
+++ src/main/java/org/example/chapter1/Queue.java
@@ -1,18 +1,53 @@
 package org.example.chapter1;
 
+import org.w3c.dom.Node;
+
 import java.util.Iterator;
 
 public class Queue <Item> implements Iterable<Item>{
+    private Node first;
+    private Node last;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
 
     public Queue(){}
 
-    public void enqueue(Item item){}
-
-    public Item dequeue(){
-        return null;
+    public boolean isEmpty(){
+        return first == null;
     }
 
-    public int size(){return 0;}
+    public void enqueue(Item item){
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()){
+            first = last;
+        }else{
+            oldLast.next = last;
+        }
+        N++;
+    }
+
+    public Item dequeue(){
+        if (isEmpty()){
+            return null;
+        }
+        Item item = first.item;
+        first = first.next;
+        // 这里容易遗漏
+        if (isEmpty()){
+            last = null;
+        }
+        N--;
+        return item;
+    }
+
+    public int size(){return N;}
 
     @Override
     public Iterator<Item> iterator() {
--- src/main/java/org/example/chapter1/Stack.java
+++ src/main/java/org/example/chapter1/Stack.java
@@ -1,18 +1,40 @@
 package org.example.chapter1;
 
+import org.w3c.dom.Node;
+
 import java.util.Iterator;
 
 public class Stack<Item> implements Iterable<Item> {
+
+    private Node first;
+    private int N;
+
+
+    private class Node{
+        Item item;
+        Node next;
+    }
+
     public Stack(){}
 
-    void push(Item item) {}
+    public int size(){return N;}
 
+    public void push(Item item)  {
+        Node oldFirst = first;
+        first = new Node();
+        first.next = oldFirst;
+        first.item = item;
+        N++;
+    }
     public Item pop() {
-        return null;
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
     }
 
     public boolean isEmpty() {
-        return false;
+        return first == null;
     }
 
     @Override
