c757051b4a1a874d756a05bab4305cc26e1722a7
renpeng <renpeng@hellogroup.com>
2024-05-09 11:48:43 +0800

    add

---
+++ src/main/java/org/example/sort/MaxPQ.java
@@ -0,0 +1,61 @@
+package org.example.sort;
+
+public class MaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N = 0;
+
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
+    }
+
+    public  boolean isEmpty(){
+        return N == 0;
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public void insert(Key key){
+        pq[++N] = key;
+        swim(N);
+    }
+
+    public Key delMax(){
+        Key max = pq[1];
+        pq[1] = pq[N];
+        pq[N] = null;
+        sink(1);
+        return max;
+    }
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exch(k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (k * 2 <= N) {
+            int j = k * 2;
+            if (j < N && less(j, j + 1)) {
+                j++;
+            }
+            if (!less(k, j)) {
+                break;
+            }
+            exch(k, j);
+        }
+    }
+
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+    }
+}
