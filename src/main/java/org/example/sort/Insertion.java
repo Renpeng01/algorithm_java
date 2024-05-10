26214d205bb8c5ccacfe776bcce2dbc0679aafec
renpeng <renpeng@hellogroup.com>
2024-05-10 15:06:02 +0800

    add

--- src/main/java/org/example/sort/Insertion.java
+++ src/main/java/org/example/sort/Insertion.java
@@ -39,6 +39,16 @@ public class Insertion {
         }
     }
 
+    public static void sort3(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int j = i;
+            for (;j > 0 && less(a[j], a[j - 1]); j--) {
+                a[j] = a[j-1];
+            }
+            a[j] = a[i];
+        }
+    }
+
     private static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
