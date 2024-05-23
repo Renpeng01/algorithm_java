0a4cc6a2e71187a1bb8ba82dc99f6c67fefddf16
renpeng <renpeng@hellogroup.com>
2024-05-23 09:57:51 +0800

    add

--- src/main/java/org/example/graph/Cycle.java
+++ src/main/java/org/example/graph/Cycle.java
@@ -14,12 +14,15 @@ public class Cycle {
         }
     }
 
+
     private void dfs(Graph G, int v, int u) {
         marked[v] = true;
         for (int w : G.adj(v)) {
             if (!marked[w]) {
                 dfs(G, w, v);
-            }else if (w != u){
+            } else if (w != u) {
+//                使用 DFS 可以判断一个无向图和有向中是否存在环。深度优先遍历图，如果在遍历的过程中，
+//                发现某个结点有一条边指向已访问过的结点，并且这个已访问过的结点不是上一步访问的结点，则表示存在环
                 hasCycle = true;
             }
         }
