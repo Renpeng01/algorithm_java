2f0d93f8c61f921058a62f09532494325e2e0409
renpeng <renpeng@hellogroup.com>
2024-05-23 11:13:22 +0800

    add

---
+++ src/main/java/org/example/graph/Digraph.java
@@ -0,0 +1,49 @@
+package org.example.graph;
+
+import edu.princeton.cs.algs4.Bag;
+
+public class
+Digraph {
+
+    private final int V;
+    private int E;
+
+    private Bag<Integer>[] adj;
+
+    public Digraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    // 实现逻辑为反向构造一次图
+    public Digraph reverse() {
+        Digraph R = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj[v]) {
+                R.addEdge(w, v);
+            }
+        }
+        return R;
+    }
+}
