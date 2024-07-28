6f89f1896c49783a4e29481334890da64cae4e05
renpeng <renpeng@p1.com>
2024-07-28 15:54:51 +0800

    add

--- src/main/java/org/example/sort/Merger.java
+++ src/main/java/org/example/sort/Merger.java
@@ -5,14 +5,14 @@ public class Merger extends Example {
     private static Comparable[] aux;
 
     // 自底向上
-    public static void sort1(Comparable[] a){
-        aux = new Comparable[a.length];
-        for(int sz = 1; sz < a.length; sz = sz + sz){
-            for(int lo = 0; lo <a.length-sz; lo += sz){
-                merge(a,lo,lo+sz-1, Math.min(lo + sz + sz-1,a.length-1));
-            }
-        }
-    }
+//    public static void sort1(Comparable[] a){
+//        aux = new Comparable[a.length];
+//        for(int sz = 1; sz < a.length; sz = sz + sz){
+//            for(int lo = 0; lo <a.length-sz; lo += sz){
+//                merge(a,lo,lo+sz-1, Math.min(lo + sz + sz-1,a.length-1));
+//            }
+//        }
+//    }
 
     // 自顶向下
     public static void sort(Comparable[] a) {
@@ -30,20 +30,18 @@ public class Merger extends Example {
 
 
         // 如果a[mid] 小于等于 a[mid+1] 则2个、部分已经是有序的，不需要merger了
-        if (less(a[mid+1],a[mid])){
-            merge(a, lo, mid, hi);
-        }
+        merge(a, lo, mid, hi);
     }
 
     public static void merge(Comparable[] a, int lo, int mid, int hi) {
         int i = lo;
         int j = mid + 1;
 
-        for (int k = 0; k < a.length; k++) {
-            aux[k] = a[i];
+        for (int k = 0; k <= hi; k++) {
+            aux[k] = a[k];
         }
 
-        for (int k = lo; k < hi; k++) {
+        for (int k = lo; k <= hi; k++) {
             if (i > mid) {
                 a[k] = aux[j++];
             } else if (j > hi) {
@@ -54,6 +52,14 @@ public class Merger extends Example {
                 a[k] = aux[i++];
             }
         }
+    }
+
+    public static void main(String[]args){
+        String[] a = {"E", "A", "S", "Y", "Q", "U", "E", "S", "T", "I", "O", "N"};
+        show(a);
+        sort(a);
+        show(a);
+//        assert isSorted(a);
     }
 }
 // 对于长度为N的任意数组，自动乡下的归并排序需要 NlgN/2~ NlogN次比较
@@ -67,5 +73,7 @@ public class Merger extends Example {
 // 效率：插入排序在数据基本有序时效率高，选择排序无论数据如何分布，效率都较低。
 // 稳定性：插入排序是稳定的，选择排序是不稳定的。
 // 元素移动：插入排序可能需要更多的元素移动，但选择排序每次只进行一次交换
+
+// 自底向上的归并排序比较适合用链表组织的数据，只需要重组链表链接就能将链表原地排序
 
 
