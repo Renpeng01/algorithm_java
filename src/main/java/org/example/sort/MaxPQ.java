285cdfc780e20265666c379d2f447165c251981d
renpeng <renpeng@p1.com>
2024-08-04 17:39:02 +0800

    add

--- src/main/java/org/example/sort/MaxPQ.java
+++ src/main/java/org/example/sort/MaxPQ.java
@@ -1,62 +1,73 @@
 package org.example.sort;
 
-public class MaxPQ <Key extends Comparable<Key>> {
+public class MaxPQ<Key extends Comparable<Key>> {
     private Key[] pq;
     private int N;
 
-    public MaxPQ(int maxN){
-        pq = (Key[]) new Comparable[maxN+1];
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return N == 0;
     }
 
-    public int size(){
+    public int size() {
         return N;
     }
 
-    public void insert(Key v){
+    public void insert(Key v) {
         pq[++N] = v;
         swim(N);
     }
 
-    public Key delMax(){
+    public Key delMax() {
         Key max = pq[1];
         exch(1, N--);
-        pq[N+1] = null;
+        pq[N + 1] = null;
         sink(1);
         return max;
     }
 
-    private boolean less(int i,int j){
+    private boolean less(int i, int j) {
         return pq[i].compareTo(pq[j]) < 0;
     }
 
-    private void exch(int i, int j){
+    private void exch(int i, int j) {
         Key t = pq[i];
         pq[i] = pq[j];
         pq[j] = t;
     }
 
-    private void swim(int k){
-        while (k > 1 && less(k/2,k)){
-            exch(k/2,k);
-            k = k/2;
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exch(k / 2, k);
+            k = k / 2;
         }
     }
 
-    private void sink(int k){
-        while (2*k <=N){
+    private void sink(int k) {
+        while (2 * k <= N) {
             int j = k * 2;
-            if (j < N && less(j,j+1)){
+            if (j < N && less(j, j + 1)) {
                 j++;
             }
-            if (!less(k,j)){
+            if (!less(k, j)) {
                 break;
             }
-            exch(k,j);
+            exch(k, j);
             k = j;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = {2, 1, 4, 3, 6, 5, 9, 8, 7};
+        MaxPQ<Integer> m = new MaxPQ<>(10);
+        for (int i = 0; i < a.length; i++) {
+            m.insert(a[i]);
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(m.delMax());
         }
     }
 }
